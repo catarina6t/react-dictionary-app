88e246f53f05cc4a5d83c361a20d84980bfbf851
catarina6t <catarinatorres@live.com.pt>
2021-12-03 13:50:10 +0000

    Fix Error Commit

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,37 +7,29 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
 
-
   function handleResponse(response) {
-    console.log(response.data[0]);
-    //update this result state everytime get a response from a API
     setResults(response.data[0]);
-
     //console.log(response.data[0].meanings[0].definitions[0].definition);
-  
-}
+  }
 
   function search(event) {
     event.preventDefault();
-    //alert(`Searching for ${keyword} definition`);
-    // documentation : https://dictionaryapi.dev/
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
-
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
-      <Results results ={results } />
-      //send this to the component results
+      <Results results={results} />
     </div>
   );
 }
